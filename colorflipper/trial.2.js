72e6e2f2ac364e5782cf3c8b96ea430d2d5c016f
Himanshu Chhatwal <himanshuchhatwal9295@gmail.com>
2023-01-19 10:56:30 +0530

    color flipper done

--- colorflipper/trial.2.js
+++ colorflipper/trial.2.js
@@ -2,14 +2,13 @@ const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 const btn = document.getElementById('btn');
 const color = document.querySelector('.color');
 btn.addEventListener('click',function(){
-    let hexColor = '#';
-    for(let i =0;i<6;i++){
-        hexColor += hex[getRandomNumber()];
-    }
-    color.textContent = hexColor;
-    document.body.style.backgroundColor = hexColor; 
+    let code = "#";
+    for (let i=0;i<6;i++){
+        code += hex[randomNum()];
+    };
+    color.textContent = code;
+    document.body.style.backgroundColor = code;
 });
-
-function getRandomNumber (){
+function randomNum(){
     return Math.floor(Math.random()*hex.length)
 }
